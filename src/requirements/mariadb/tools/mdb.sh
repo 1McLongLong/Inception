3bfb3ff7b13d2ded1de2baf084d931cda8637d81
1McLongLong <taha.ouahmane@gmail.com>
2024-12-27 00:51:14 +0100

    mariadb script almost finished

--- src/requirements/mariadb/tools/mdb.sh
+++ src/requirements/mariadb/tools/mdb.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+
+systemctl start mariadb
+
+sleep 5
 
 
+mariadb -e "CrEaTe DaTaBaSe If NoT ExIsTs \'${MARIADB_NM}\';"
 
+mariadb -e "CrEaTe UsEr If NoT ExIsTs \'${MARIADB_USER}\'@'%' IdEnTiFiEd By \'${MARIADB_PW}\';"
 
+mariadb -e "GrAnT aLl PrIvILegEs On ${MARIADB_NM}.* tO \'${MARIADB_USER}\'@'%';"
 
+mariadb -e "fLuSh pRiViLeGeS;"
 
+mariadbadmin -u root -p $MARIADB_R_PW shutdown
 
 
 
