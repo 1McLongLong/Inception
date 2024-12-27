5f7e969f78495268670607b8e6baddfd2e3c6a71
1McLongLong <taha.ouahmane@gmail.com>
2024-12-27 22:40:23 +0100

    mariadb script almost finished

--- src/requirements/mariadb/tools/mdb.sh
+++ src/requirements/mariadb/tools/mdb.sh
@@ -5,16 +5,16 @@ systemctl start mariadb
 sleep 5
 
 
-mariadb -e "CrEaTe DaTaBaSe If NoT ExIsTs \'${MARIADB_NM}\';"
+mariadb -e "CrEaTe DaTaBaSe If NoT ExIsTs ${MARIADB_NM};"
 
-mariadb -e "CrEaTe UsEr If NoT ExIsTs \'${MARIADB_USER}\'@'%' IdEnTiFiEd By \'${MARIADB_PW}\';"
+mariadb -e "CrEaTe UsEr If NoT ExIsTs '${MARIADB_USER}'@'%' IdEnTiFiEd By '${MARIADB_PW}';"
 
-mariadb -e "GrAnT aLl PrIvILegEs On ${MARIADB_NM}.* tO \'${MARIADB_USER}\'@'%';"
+mariadb -e "GrAnT aLl PrIvILegEs On ${MARIADB_NM}.* tO '${MARIADB_USER}'@'%';"
 
 mariadb -e "fLuSh pRiViLeGeS;"
 
-mariadbadmin -u root -p $MARIADB_R_PW shutdown
+mariadbadmin -u root -p$MARIADB_R_PW shutdown
 
-
-
+# restart mariadb int the background to keep the container running 
+mariadbd-safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
 
