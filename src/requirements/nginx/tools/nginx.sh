1f7244577d79bf361fe02c0dbb287de61dec98fa
1McLongLong <taha.ouahmane@gmail.com>
2025-01-01 00:21:06 +0100

    nginx almost done

---
+++ src/requirements/nginx/tools/nginx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "server {
+    listen 443 ssl;
+    server_name www.touahman.42.fr touahman.42.fr;
+
+    ssl_certificate /etc/ssl/private/nginx-selfsigned.crt;
+    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+    ssl_protocols TLSv1.3;
+
+    index index.php;
+    root /var/www/html;
+
+    location ~ \.php$ {
+              include snippets/fastcgi-php.conf;
+              fastcgi_pass wordpress:9000;
+    }
+  }" >> /etc/nginx/sites-available/default
+
+nginx -g "daemon off;"
