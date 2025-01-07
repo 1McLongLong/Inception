c0304ae3d0b206fc5c968deb2384bc9e77dae429
mclonglong <taha.ouahmane@gmail.com>
2025-01-08 00:50:38 +0100

    sss

--- src/requirements/mariadb/tools/mdb.sh
+++ src/requirements/mariadb/tools/mdb.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# service mariadb start 
 mysqld_safe &
 sleep 5
-#
-# mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_NM}\`;"
-#
-# mariadb -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PW}';"
-#
-# mariadb -e "GRANT ALL PRIVILEGES ON \`${MARIADB_NM}\`.* TO '${MARIADB_USER}'@'%';"
-#
-# mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_R_PW';"
-#
-# mariadb -e "FLUSH PRIVILEGES;"
 
 mariadb -u root <<EOF
 CREATE DATABASE $MARIADB_NM;
